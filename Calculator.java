719cf205037d385e37ef3e9b26a0dd28cbd0eba7
sdim-ua <ingersol@yandex.ua>
2019-02-03 14:09:49 +0400

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+
+/* 
+Калькулятор
+
+
+*/
+
+public class Calculator {
+    public static int plus(int a, int b) {
+        //напишите тут ваш код
+        return a + b ;
+    }
+
+    public static int minus(int a, int b) {
+        //напишите тут ваш код
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        //напишите тут ваш код
+        return a * b;
+    }
+
+    public static double division(int a, int b) {
+        //напишите тут ваш код
+        return a *1.0 / b;
+       
+    }
+
+    public static double percent(int a, int b) {
+        //напишите тут ваш код
+        double c = a / 100.0;
+        double d =   a * b;
+        
+        return d;
+    }
+
+    public static void main(String[] args) {
+    
+    
+
+    }
+}
